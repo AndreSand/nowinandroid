35e21349c7625ef56127c54534b8b9fb6245956a
Andres Sandoval <sandovalandres@google.com>
2023-10-05 10:26:10 -0700

    Update Firebase Test lab device https://firebase.google.com/docs/test-lab/android/available-testing-devices

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,8 +77,8 @@ android {
 
         managedDevices {
             create("myFtlDevice") {
-                device = "Pixel2"
-                apiLevel = 29
+                device = "Pixel7"
+                apiLevel = 33
             }
         }
     }
